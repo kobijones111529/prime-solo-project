35c4ba2a8cba91db49e06496e0023554c1669881
Kobi Jones <kobijonesmail@gmail.com>
2023-05-22 15:55:14 -0500

    Add setup SQL to database.sql file

--- database.sql
+++ database.sql
@@ -1,10 +1,29 @@
+--
+-- Setup
+--
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TYPE "post_type" AS ENUM ('offer', 'request');
+
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR(80) UNIQUE NOT NULL,
+	"password" TEXT NOT NULL
+);
+
+CREATE TABLE "posts" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL REFERENCES "users" ON DELETE CASCADE,
+	"type" "post_type" NOT NULL,
+	"plant_name" VARCHAR(127) NOT NULL,
+	"image_url" TEXT,
+	"description" TEXT,
+	"latitude" NUMERIC(8,6) NOT NULL,
+	"longitude" NUMERIC(9,6) NOT NULL,
+	"contact_url" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "users_interested_posts" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL REFERENCES "users",
+	"post_id" INT NOT NULL REFERENCES "posts" ON DELETE CASCADE
 );
