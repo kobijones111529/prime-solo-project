a57aa9a89394e29d5709bfaa3efc122453f53513
Kobi Jones <kobijonesmail@gmail.com>
2023-06-22 15:36:29 -0500

    Change port env var name

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get("*", (_req, res) => {
 });
 
 // App Set //
-const PORT = process.env["SERVER_PORT"] || 5000;
+const PORT = process.env["PORT"] || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
